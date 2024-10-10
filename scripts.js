33e0564fea4c0486c31cabf5b028091e85616d75
KlakCodes <harrisonjclark@hotmail.com>
2024-10-10 15:53:06 +0100

    Created fill library function
    Function fills the library with example books.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 const myLibrary = [];
 
-function Book (title, author, pages, read) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
+    this.info = function () {
         let readMessage;
-        if(this.read) {
+        if (this.read) {
             readMessage = 'read'
         } else {
             readMessage = 'not read yet'
@@ -17,8 +17,30 @@ function Book (title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  // do stuff here
+
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, true);
+// TEST START
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
+const ofMiceAndMen = new Book('Of Mice and Men', 'John Steinbeck', 144, true);
+const animalFarm = new Book('Animal Farm', 'George Orwell', 176, true);
 console.log(theHobbit.info());
+
+myLibrary.push(theHobbit);
+myLibrary.push(ofMiceAndMen);
+myLibrary.push(animalFarm);
+
+function displayLibrary() {
+    var table = document.querySelector("table");
+
+    myLibrary.forEach(value => {
+        var row = table.insertRow();
+        row.insertCell(0).innerHTML = value.title;
+        row.insertCell(1).innerHTML = value.author;
+        row.insertCell(2).innerHTML = value.pages;
+        row.insertCell(3).innerHTML = value.read;
+    });
+}
+
+displayLibrary();
+// TEST END

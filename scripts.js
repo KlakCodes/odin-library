1b63400c77f57395650a1dcbdc4b4b84daa2a3c8
KlakCodes <harrisonjclark@hotmail.com>
2024-10-15 16:39:07 +0100

    Created book cards and function to create them

--- scripts.js
+++ scripts.js
@@ -20,27 +20,45 @@ function addBookToLibrary() {
 
 }
 
-// TEST START
+// TEST 1 START
 const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
 const ofMiceAndMen = new Book('Of Mice and Men', 'John Steinbeck', 144, true);
 const animalFarm = new Book('Animal Farm', 'George Orwell', 176, true);
+const hamlet = new Book('Hamlet', 'Shakespeare', 192, false)
 console.log(theHobbit.info());
 
 myLibrary.push(theHobbit);
 myLibrary.push(ofMiceAndMen);
 myLibrary.push(animalFarm);
+myLibrary.push(hamlet);
+
+// function displayLibrary() {
+//     var table = document.querySelector("table");
+
+//     myLibrary.forEach(value => {
+//         var row = table.insertRow();
+//         row.insertCell(0).innerHTML = value.title;
+//         row.insertCell(1).innerHTML = value.author;
+//         row.insertCell(2).innerHTML = value.pages;
+//         row.insertCell(3).innerHTML = value.read;
+//     });
+// }
+
+// displayLibrary();
+// TEST 1 END
+
+// TEST 2 START
 
 function displayLibrary() {
-    var table = document.querySelector("table");
+    const library = document.querySelector(".libraryContainer");    
 
     myLibrary.forEach(value => {
-        var row = table.insertRow();
-        row.insertCell(0).innerHTML = value.title;
-        row.insertCell(1).innerHTML = value.author;
-        row.insertCell(2).innerHTML = value.pages;
-        row.insertCell(3).innerHTML = value.read;
-    });
+        const div = document.createElement("div");
+        div.classList.toggle("book");
+        div.textContent = `${value.title}\n${value.author}\n${value.pages}`;
+        library.appendChild(div);
+    })
 }
 
 displayLibrary();
-// TEST END
+// TEST 2 END

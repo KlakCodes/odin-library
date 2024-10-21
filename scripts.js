8889893afc2e4e2f490bb41930e21cacbe61a930
KlakCodes <harrisonjclark@hotmail.com>
2024-10-21 16:42:33 +0100

    Refined the addBookToLibrary funtion
    Split the adding of the book to the array and the displaying to separate functions

--- scripts.js
+++ scripts.js
@@ -20,33 +20,31 @@ const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
 const ofMiceAndMen = new Book('Of Mice and Men', 'John Steinbeck', 144, true);
 const animalFarm = new Book('Animal Farm', 'George Orwell', 176, true);
 const hamlet = new Book('Hamlet', 'Shakespeare', 192, false);
-console.log(theHobbit.info());
 
 myLibrary.push(theHobbit);
 myLibrary.push(ofMiceAndMen);
 myLibrary.push(animalFarm);
 myLibrary.push(hamlet);
 
-function displayLibrary() {
-    const library = document.querySelector(".libraryContainer");    
-
+function displayLibrary() { 
     myLibrary.forEach(value => {
-        const div = document.createElement("div");
-        div.classList.toggle("book");
-        div.textContent = `${value.title}\n${value.author}\n${value.pages}`;
-        library.appendChild(div);
+        displayBook(value);
     })
 }
 
-function addBookToLibrary(title, author, pages, read) {
+function displayBook(book) {
     const library = document.querySelector(".libraryContainer");
+    const div = document.createElement("div");
+        div.classList.toggle("book");
+        div.textContent = `${book.title}\n${book.author}\n${book.pages}`;
+        library.appendChild(div);
+}
+
+function addBookToLibrary(title, author, pages, read) {
     var newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 
-    const div = document.createElement("div");
-    div.classList.toggle("book");
-    div.textContent = `${newBook.title}\n${newBook.author}\n${newBook.pages}`;
-    library.appendChild(div);
+    displayBook(newBook);
 
     dialog.close();
 }
@@ -69,7 +67,6 @@ submitBook.addEventListener('click', function(event) {
     var pages = document.querySelector('#pages').value;
     var read = document.querySelector('#read').value;
     addBookToLibrary(title, author, pages, read);
-    console.log("Added");
 })
 
 closeDialog.addEventListener('click', () => {
